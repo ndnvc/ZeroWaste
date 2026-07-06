0a6d54e1ea0b91f746440fff24d1f22f0ea1fa00
Jesvally <jsvlly@gmail.com>
2026-07-06 19:22:42 +0700

    try

--- app/src/main/java/com/example/zerowaste/MainActivity.kt
+++ app/src/main/java/com/example/zerowaste/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.zerowaste
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import com.example.zerowaste.ui.login.LoginScreen
 import com.example.zerowaste.ui.theme.ZeroWasteTheme
 
@@ -11,7 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ZeroWasteTheme {
-                LoginScreen()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen()
+                }
             }
         }
     }
